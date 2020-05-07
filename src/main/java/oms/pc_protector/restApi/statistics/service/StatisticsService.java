a712ea53aa01f9f28d21d6d1e63ed8d1d2775b6d
hozzi <hozzi@omsecurity.kr>
2020-05-07 14:37:51 +0900

    pull for static3

--- src/main/java/oms/pc_protector/restApi/statistics/service/StatisticsService.java
+++ src/main/java/oms/pc_protector/restApi/statistics/service/StatisticsService.java
@@ -83,13 +83,19 @@ public class StatisticsService {
 
             if(runPc > 0) avgScore = avgScore / runPc;
 
+            log.info("-----------------------------");
+            log.info("부서 이름 : " + department.getName());
+            log.info("평균 점수 : " + avgScore);
+            log.info("전체 PC 수 : " + totalPc);
+            log.info("실행 PC 수 : " + runPc);
+            log.info("-----------------------------");
 
             for (int i = 0; i < safePc.length; i++) {
                 safePcDivideAllPc[i] = (int) Math.round(
-                        ((int) safePc[i] / (double) totalPc) * 100);
+                        ((int) safePc[i] / (double) totalPc));
 
                 safePcDivideRunPc[i] = runPc == 0 ? 0 : (int) Math.round(
-                        ((int) safePc[i] / (double) runPc) * 100);
+                        ((int) safePc[i] / (double) runPc));
             }
 
             objectMap.put("departmentName", department.getName());       // 부서 이름
