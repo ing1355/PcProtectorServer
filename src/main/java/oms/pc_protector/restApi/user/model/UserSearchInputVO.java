133c05ef7c47b1fac286b5f909fdc3949a3f4c09
seob <seob@omsecurity.kr>
2020-05-13 14:53:17 +0900

    UserSearchInputVo 추가

---
+++ src/main/java/oms/pc_protector/restApi/user/model/UserSearchInputVO.java
@@ -0,0 +1,15 @@
+package oms.pc_protector.restApi.user.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSearchInputVO {
+
+    private String userId;
+    private String name;
+    private String phone;
+    private Integer departmentCode;
+
+}
