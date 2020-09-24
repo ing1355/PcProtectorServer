53a2ce17548516f967cb66093bfaf3a34c48e3b3
hozzi <hozzi@omsecurity.kr>
2020-09-24 09:42:07 +0900

    license push

--- src/main/java/oms/pc_protector/PcProtectorApplication.java
+++ src/main/java/oms/pc_protector/PcProtectorApplication.java
@@ -1,31 +1,28 @@
 package oms.pc_protector;
 
-import lombok.extern.java.Log;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.*;
 
 
 @SpringBootApplication
 @EnableScheduling
 public class PcProtectorApplication {
-//    static {
-//        System.loadLibrary("ex");
-//    }
-//
-//    private static native boolean CompareLicenseKey();
+    public static void main(String[] args) throws IOException {
+        File file = new File("./oms.lic");
+        FileReader filereader = new FileReader(file);
+        //입력 버퍼 생성
+        BufferedReader bufReader = new BufferedReader(filereader);
+        String line = "";
+        while ((line = bufReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        //.readLine()은 끝에 개행문자를 읽지 않는다.
+        bufReader.close();
 
-    public static void main(String[] args) {
-//        if(CompareLicenseKey()) {
-            SpringApplication.run(PcProtectorApplication.class, args);
-//        } else {
-//            System.exit(0);
-//        }
+        SpringApplication.run(PcProtectorApplication.class, args);
     }
 
 }
