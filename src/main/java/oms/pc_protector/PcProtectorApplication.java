1f5511c05c966b7b5757d9d9f66162fc6ddf5e82
kimjiho <hozzi@omsecurity.kr>
2020-09-24 18:55:38 +0900

    라이센스 적용

--- src/main/java/oms/pc_protector/PcProtectorApplication.java
+++ src/main/java/oms/pc_protector/PcProtectorApplication.java
@@ -2,6 +2,8 @@ package oms.pc_protector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.*;
@@ -10,17 +12,7 @@ import java.io.*;
 @SpringBootApplication
 @EnableScheduling
 public class PcProtectorApplication {
-    public static void main(String[] args) throws IOException {
-        File file = new File("./oms.lic");
-        FileReader filereader = new FileReader(file);
-        //입력 버퍼 생성
-        BufferedReader bufReader = new BufferedReader(filereader);
-        String line = "";
-        while ((line = bufReader.readLine()) != null) {
-            System.out.println(line);
-        }
-        //.readLine()은 끝에 개행문자를 읽지 않는다.
-        bufReader.close();
+    public static void main(String[] args) {
 
         SpringApplication.run(PcProtectorApplication.class, args);
     }
---
+++ src/main/java/oms/pc_protector/util/License.java
@@ -0,0 +1,82 @@
+package oms.pc_protector.util;
+
+import oms.pc_protector.PcProtectorApplication;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+@Component
+public class License implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        File file = new File("/Users/licenseKey/oms.lic");
+        FileReader filereader = new FileReader(file);
+        //입력 버퍼 생성
+        BufferedReader bufReader = new BufferedReader(filereader);
+        String line = "";
+        String product = "", domain = "", input_id = "";
+        while ((line = bufReader.readLine()) != null) {
+            if(line.contains("product")) {
+                product = line.split("=")[1];
+            } else if(line.contains("domain")) {
+                domain = line.split("=")[1];
+            } else if(line.contains("id")) {
+                input_id = line.split("=")[1];
+            }
+        }
+        //.readLine()은 끝에 개행문자를 읽지 않는다.
+        bufReader.close();
+
+        String hash_product = DigestUtils.sha512Hex(product);
+        hash_product = DigestUtils.sha512Hex(hash_product);
+        String hash_domain = DigestUtils.sha512Hex(domain);
+        hash_domain = DigestUtils.sha512Hex(hash_domain);
+        String hash_mac = DigestUtils.sha512Hex(getHardwareAddress());
+        hash_mac = DigestUtils.sha512Hex(hash_mac);
+        String hash_uuid = DigestUtils.sha512Hex(hash_product.substring(1,2) + hash_domain.substring(1,2) + hash_mac.substring(1,2));
+        hash_uuid = DigestUtils.sha512Hex(hash_uuid);
+
+        String id = DigestUtils.sha512Hex(hash_product + hash_domain + hash_mac + hash_uuid);
+        id = DigestUtils.sha512Hex(id);
+
+        System.out.println("mac : " + getHardwareAddress());
+        System.out.println("input_id : " + input_id);
+        System.out.println("id : " + id);
+        if(!input_id.equals(id)) {
+            System.exit(0);
+        }
+    }
+
+    public static String getHardwareAddress() throws UnknownHostException,
+            SocketException
+    {
+        InetAddress ipAddress = InetAddress.getLocalHost();
+        NetworkInterface networkInterface = NetworkInterface
+                .getByInetAddress(ipAddress);
+        byte[] macAddressBytes = networkInterface.getHardwareAddress();
+        StringBuilder macAddressBuilder = new StringBuilder();
+
+        for (int macAddressByteIndex = 0; macAddressByteIndex < macAddressBytes.length; macAddressByteIndex++)
+        {
+            String macAddressHexByte = String.format("%02X",
+                    macAddressBytes[macAddressByteIndex]);
+            macAddressBuilder.append(macAddressHexByte);
+
+            if (macAddressByteIndex != macAddressBytes.length - 1)
+            {
+                macAddressBuilder.append(":");
+            }
+        }
+
+        return macAddressBuilder.toString();
+    }
+}
